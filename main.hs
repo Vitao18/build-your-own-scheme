b7b4301725d036023299021d1f228b519c71f3d6
Vitao18 <victoronline9@gmail.com>
2020-11-20 12:56:08 -0300

    added some functions to understand better IO

---
+++ main.hs
@@ -0,0 +1,18 @@
+module Main where
+import System.Environment
+
+{- main :: IO ()
+main = do
+  args <- getArgs
+  putStrLn ("Hello, " ++ args !! 0 ++ " " ++ args !! 1) -}
+
+{- main :: IO ()
+main = do
+  args <- getArgs
+  putStrLn $ show (2 * (read (args !! 0) :: Int)) -}
+
+{- main :: IO ()
+main = do
+  putStrLn "Enter something and we will make it rocks!"
+  input <- getLine
+  putStrLn $ input ++ "!!" -}
