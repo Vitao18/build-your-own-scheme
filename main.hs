51e9800623e71e39e6299be79d0a6f6ddd255f01
Vitao18 <victoronline9@gmail.com>
2020-11-20 17:36:36 -0300

    create do-monad and binding operator versions of parse number function

--- main.hs
+++ main.hs
@@ -1,5 +1,62 @@
 module Main where
+import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+import Control.Monad
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest  <- many (letter <|> symbol <|> digit)
+  let atom = first:rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _    -> Atom atom
+
+{- parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit -}
+
+{- parseNumber :: Parser LispVal
+parseNumber = do
+  x <- many1 digit
+  return $ Number (read x) -}
+
+parseNumber :: Parser LispVal
+parseNumber =  (many1 digit) >>= (return . Number . read)
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom <|> parseString <|> parseNumber
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right val -> "Found Value"
+
+main :: IO ()
+main = do
+  (expr:_) <- getArgs
+  putStrLn $ readExpr expr
+
 
 {- main :: IO ()
 main = do
@@ -15,4 +72,5 @@ main = do
 main = do
   putStrLn "Enter something and we will make it rocks!"
   input <- getLine
-  putStrLn $ input ++ "!!" -}
+putStrLn $ input ++ "!!" -}
+
